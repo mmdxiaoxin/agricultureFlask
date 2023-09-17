630b3938b860145dfa1281319de91e648e15dc36
小新 <782446723@qq.com>
2023-09-17 20:45:22 +0800

    使用数据库连接池防止数据库连接重用等问题，修复代码结构。

--- app.py
+++ app.py
@@ -1,40 +1,47 @@
 import json
 import time
-from datetime import datetime
-
 import pymysql
 import requests
 from flask import Flask, request, Blueprint, jsonify
 from flask_cors import CORS
 
-from camera_get import CameraUrl
-from camera_get import Camera_Control_Start
-from camera_get import Camera_Control_Stop
-
 app = Flask(__name__)
-
 cors = CORS(app)
-# 主机名
-HOSTNAME = "127.0.0.1"
-# 端口号
-PORT = 3306
-# 账号
-USERNAME = "root"
-# 密码
-PASSWORD = "122600"
-# 数据库
-DATABASE = "eviroment_data"
-# 线程池
-THREADS = {}
-# 线程池是一个字典，key为设备id，value为线程对象
 
-# 使用pymysql创建数据库连接
-global con
-con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                      database=DATABASE)
+# 数据库连接参数
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "122600",
+    "database": "eviroment_data",
+    "charset": "utf8",
+}
+
+# 最大连接数
+MAX_DB_CONNECTIONS = 10
+
+# 全局数据库连接池
+db_connections = []
 
 # 创建名为"agriculture_bp"的Blueprint，并指定前缀
 agriculture_bp = Blueprint('agriculture', __name__, url_prefix='/agriculture')
+
+
+# 函数用于获取数据库连接
+def get_db_connection():
+    if len(db_connections) < MAX_DB_CONNECTIONS:
+        conn = pymysql.connect(**DB_CONFIG)
+        db_connections.append(conn)
+        return conn
+    else:
+        # 如果连接池已满，等待并尝试重新获取连接
+        for conn in db_connections:
+            if not conn.open:
+                conn.ping(reconnect=True)
+                return conn
+        time.sleep(1)  # 等待1秒后重试
+        return get_db_connection()
 
 
 @agriculture_bp.route('/', methods=['GET'])
@@ -82,93 +89,11 @@ def exportData():
     return jsonify({"code": 200, "message": "成功"})
 
 
-# 返回所有地区列表
-@agriculture_bp.route("/address/select", methods=['GET'])
-def Address_Select():
-    global con
-    if not con.ping():
-        con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                              database=DATABASE)
-    cur = con.cursor()
-    affected_rows = cur.execute(
-        'SELECT * FROM address;'
-    )
-    con.commit()
-    results = cur.fetchall()
-    cur.close()
-    rows = []
-    for row in results:
-        id = row[0]
-        name = row[1]
-
-        row_dict = {
-            'id': id,
-            'name': name
-        }
-
-        rows.append(row_dict)
-
-    # 构建响应数据
-    response_data = {
-        'code': 200,  # 可根据需要设置不同的状态码
-        'data': rows,
-        'message': 'Success'  # 可根据需要设置不同的消息
-    }
-
-    # 使用 Flask 的 jsonify 函数将响应数据转换为 JSON 格式并返回
-    return jsonify(response_data)
-
-
-# 返回传感器设备列表（根据地区）
-@agriculture_bp.route("/device/select", methods=['GET'])
-def Device_Select():
-    global con
-    if not con.ping():
-        con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                              database=DATABASE)
-    # 创建数据库游标
-    cur = con.cursor()
-    address_id = request.args.get("address_id", default=1, type=str)
-    cur.execute("SELECT id, device_name, business_id, device_id, collect_run FROM device WHERE address_id = %s;",
-                (address_id))
-    con.commit()
-    results = cur.fetchall()
-    rows = []
-    cur.close()
-    # 遍历结果集
-    for row in results:
-        id = row[0]
-        device_name = row[1]
-        business_id = row[2]
-        device_id = row[3]
-        collect = row[4]
-
-        # 创建字典对象，表示每行结果
-        row_dict = {
-            'id': id,
-            'device_name': device_name,
-            'business_id': business_id,
-            'device_id': device_id,
-            'collect_run': collect
-        }
-        # 将每行结果的字典添加到列表中
-        rows.append(row_dict)
-
-    # 将结果列表转换为JSON格式
-    json_data = json.dumps(rows)
-    # 关闭数据库游标
-
-    return json_data
-
-
 @agriculture_bp.route("/user/deviceList", methods=['GET'])
 def getDeviceList():
-    global con
     try:
-        if not con.ping():
-            con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                                  database=DATABASE)
-        cursor = con.cursor()
+        conn = get_db_connection()
+        cursor = conn.cursor()
 
         # 查询所有站点
         cursor.execute('SELECT * FROM address;')
@@ -506,21 +431,87 @@ def mock_response():
         return jsonify({"code": 401, "message": "Unauthorized"})
 
 
-class DateTimeEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()  # 将 datetime 对象转换为 ISO 8601 格式的字符串
-        return super().default(obj)
+@agriculture_bp.route("/address/select", methods=['GET'])
+def Address_Select():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM address;')
+        results = cur.fetchall()
+        rows = []
+
+        for row in results:
+            site_id = row[0]
+            name = row[1]
+
+            row_dict = {
+                'id': site_id,
+                'name': name
+            }
+
+            rows.append(row_dict)
+
+        response_data = {
+            'code': 200,
+            'data': rows,
+            'message': 'Success'
+        }
+
+        return jsonify(response_data)
+    except Exception as e:
+        response_data = {
+            'code': 500,
+            'message': f'Server Error: {str(e)}'
+        }
+        return jsonify(response_data), 500
+    finally:
+        cur.close()
+
+
+# 返回传感器设备列表（根据地区）
+@agriculture_bp.route("/device/select", methods=['GET'])
+def Device_Select():
+    conn = get_db_connection()
+    # 创建数据库游标
+    cur = conn.cursor()
+    address_id = request.args.get("address_id", default=1, type=str)
+    cur.execute("SELECT id, device_name, business_id, device_id, collect_run FROM device WHERE address_id = %s;",
+                address_id)
+    conn.commit()
+    results = cur.fetchall()
+    rows = []
+    cur.close()
+    # 遍历结果集
+    for row in results:
+        site_id = row[0]
+        device_name = row[1]
+        business_id = row[2]
+        device_id = row[3]
+        collect = row[4]
+
+        # 创建字典对象，表示每行结果
+        row_dict = {
+            'id': site_id,
+            'device_name': device_name,
+            'business_id': business_id,
+            'device_id': device_id,
+            'collect_run': collect
+        }
+        # 将每行结果的字典添加到列表中
+        rows.append(row_dict)
+
+    # 将结果列表转换为JSON格式
+    json_data = json.dumps(rows)
+    # 关闭数据库游标
+
+    return json_data
 
 
 @agriculture_bp.route("/data/show", methods=['GET'])
 def Data_Base():
-    global con
-    if not con.ping():
-        con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                              database=DATABASE)
+    conn = get_db_connection()
     # 创建数据库游标
-    cur = con.cursor()
+    cur = conn.cursor()
     deviceId = request.args.get("deviceId", default=1, type=str)
     hour = request.args.get("hour", default=24, type=int)  # 获取 hour 参数，如果没有传递参数，默认值为 24 条数据
     columns_param = request.args.get("columns")  # 获取要选择的列，作为一个字符串
@@ -532,12 +523,12 @@ def Data_Base():
         columns_to_select = []
 
     cur.execute("SELECT name FROM env_db WHERE deviceId = %s;", (deviceId,))
-    con.commit()
+    conn.commit()
     dbname = str(cur.fetchall()[0][0]) + deviceId
 
     # 判断是否存在数据表
     cur.execute("SHOW TABLES LIKE %s;", (dbname,))
-    con.commit()
+    conn.commit()
     table_exists = cur.fetchone()
 
     if not table_exists:
@@ -549,7 +540,7 @@ def Data_Base():
         return jsonify(response_data)
 
     cur.execute("SHOW COLUMNS FROM " + dbname + ";")
-    con.commit()
+    conn.commit()
     result_columns = cur.fetchall()
     all_columns = [i[0] for i in result_columns]
 
@@ -573,7 +564,7 @@ def Data_Base():
 
     # 使用 LIMIT 子句来限制返回的数据条数
     cur.execute(f"SELECT {select_clause} FROM " + dbname + " ORDER BY id DESC LIMIT %s;", (hour,))
-    con.commit()
+    conn.commit()
     results = cur.fetchall()
     rows = []
     cur.close()
@@ -594,136 +585,17 @@ def Data_Base():
     return jsonify(response_data)
 
 
-
-@agriculture_bp.route("/camera/select", methods=['GET'])
-def Camera_Select():
-    global con
-    if not con.ping():
-        con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                              database=DATABASE)
-    # 创建数据库游标
-    cur = con.cursor()
-    address_id = request.args.get("address_id", default=1, type=str)
-    cur.execute("SELECT id, appkey, appsecret, deviceserial FROM camera WHERE address_id = %s;", address_id)
-    con.commit()
-    results = cur.fetchall()
-    rows = []
-    cur.close()
-    # 遍历结果集
-    for row in results:
-        id = row[0]
-        appkey = row[1]
-        appsecret = row[2]
-        deviceserial = row[3]
-
-        # 创建字典对象，表示每行结果
-        row_dict = {
-            'id': id,
-            'appkey': appkey,
-            'appsecret': appsecret,
-            'deviceserial': deviceserial,
-        }
-        # 将每行结果的字典添加到列表中
-        rows.append(row_dict)
-
-    # 将结果列表转换为JSON格式
-    json_data = json.dumps(rows)
-    # 关闭数据库游标
-
-    return json_data
-
-
-@agriculture_bp.route("/camera/url", methods=['GET'])
-def Camera_url():
-    global con
-    if not con.ping():
-        con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                              database=DATABASE)
-    # 创建数据库游标
-    cur = con.cursor()
-    id = request.args.get("id", default=1, type=str)
-    cur.execute("SELECT appkey, appsecret, deviceserial FROM camera WHERE id = %s;", (id))
-    con.commit()
-    results = cur.fetchall()
-    # print(results)
-    cur.close()
-    rows = []
-    for row in results:
-        appkey = row[0]
-        appsecret = row[1]
-        deviceserial = row[2]
-        # 创建字典对象，表示每行结果
-        row_dict = {
-            'appkey': appkey,
-            'appsecret': appsecret,
-            'deviceserial': deviceserial,
-        }
-        # 将每行结果的字典添加到列表中
-        rows.append(row_dict)
-    print(rows)
-    appkey = rows[0]['appkey']
-    appsecret = rows[0]['appsecret']
-    deviceserial = rows[0]['deviceserial']
-    cameraurl = CameraUrl(appkey, appsecret, deviceserial)
-    # print(cameraurl)
-    return cameraurl
-
-
-@agriculture_bp.route("/camera/control", methods=['GET'])
-def Camera_Control():
-    global con
-    if not con.ping():
-        con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                              database=DATABASE)
-    # 创建数据库游标
-    cur = con.cursor()
-    id = request.args.get("id", default=1, type=str)
-    direction = request.args.get("direction", default=1, type=int)
-    sp = request.args.get("speed", default=1, type=int)
-    cur.execute("SELECT appkey, appsecret, deviceserial FROM camera WHERE id = %s;", (id))
-    con.commit()
-    results = cur.fetchall()
-    # print(results)
-    cur.close()
-    rows = []
-    for row in results:
-        appkey = row[0]
-        appsecret = row[1]
-        deviceserial = row[2]
-        # 创建字典对象，表示每行结果
-        row_dict = {
-            'appkey': appkey,
-            'appsecret': appsecret,
-            'deviceserial': deviceserial,
-        }
-        # 将每行结果的字典添加到列表中
-        rows.append(row_dict)
-    # print(rows)
-    appkey = rows[0]['appkey']
-    appsecret = rows[0]['appsecret']
-    deviceserial = rows[0]['deviceserial']
-    control_start_res = Camera_Control_Start(appkey, appsecret, deviceserial, direction, sp)
-    print(control_start_res)
-    time.sleep(0.5)
-    control_stop_res = Camera_Control_Stop(appkey, appsecret, deviceserial)
-    print(control_stop_res)
-    return control_stop_res
-
-
 # 实时数据返回
 @agriculture_bp.route("/device/api", methods=['GET'])
 def Device_api():
-    global con
-    if not con.ping():
-        con = pymysql.connect(host='localhost', password=PASSWORD, port=PORT, user=USERNAME, charset='utf8',
-                              database=DATABASE)
+    conn = get_db_connection()
     # 创建数据库游标
-    cur = con.cursor()
+    cur = conn.cursor()
     device_id = request.args.get("id", default=1, type=str)
     method = request.args.get("method", default=1, type=str)
     cur.execute("SELECT api,business_id, device_id, equipment, version, collect_run FROM device WHERE id = %s;",
                 device_id)
-    con.commit()
+    conn.commit()
     res_device = cur.fetchone()
     if res_device is None:
         print("未查询到此设备")
@@ -738,7 +610,7 @@ def Device_api():
             "message": "没有访问权限"
         }
         return jsonify(response), 403
-    device_api = res_device[0] + '/' + method + '?' + 'Version=' + res_device[4] + '&Business=' + res_device[
+    res_device[0] + '/' + method + '?' + 'Version=' + res_device[4] + '&Business=' + res_device[
         1] + '&Equipment=' + res_device[3] + '&RequestTime=' + str(
         int(time.time())) + '&Value={ "page": 1,"length": 5,"deviceId":' + res_device[2] + '}'
     # device_api_last_index = res_api.rfind("/")
@@ -757,11 +629,8 @@ def Device_api():
     return jsonify(result)
 
 
-# 注册Blueprint到Flask应用中
-app.register_blueprint(agriculture_bp)
-
 if __name__ == "__main__":
+    # 注册Blueprint到Flask应用中
+    app.register_blueprint(agriculture_bp)
     app.run(debug=True, host="0.0.0.0", port=5000)
-    # 关闭数据库连接
-    con.close()
     print('the db is closed')
