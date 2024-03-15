594a6381024a691ab0591052e27641e9b7914d4e
小新 <782446723@qq.com>
2024-03-16 00:14:15 +0800

    refactor: 整理项目结构

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from flask_cors import CORS
 
 from model import Users, Address, db, Device
 from routes import routes_bp
+from utils import get_all_columns, get_database_name, get_columns_to_select, construct_select_clause, execute_query, \
+    convert_to_json, table_exists
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -368,47 +370,6 @@ def data_base():
             'message': '服务器错误: {}'.format(str(e))
         }
         return jsonify(response_data), 500
-
-
-def get_database_name(cur, device_id):
-    cur.execute("SELECT name FROM env_db WHERE deviceId = %s;", (device_id,))
-    return str(cur.fetchall()[0][0]) + device_id
-
-
-def table_exists(cur, dbname):
-    cur.execute("SHOW TABLES LIKE %s;", (dbname,))
-    return cur.fetchone()
-
-
-def get_all_columns(cur, dbname):
-    cur.execute("SHOW COLUMNS FROM " + dbname + ";")
-    result_columns = cur.fetchall()
-    return [i[0] for i in result_columns]
-
-
-def get_columns_to_select(columns_param, all_columns):
-    if columns_param:
-        columns_to_select = columns_param.split(',')
-    else:
-        columns_to_select = []
-    return columns_to_select if columns_to_select else all_columns
-
-
-def construct_select_clause(columns_to_select):
-    return ", ".join(columns_to_select)
-
-
-def execute_query(cur, dbname, select_clause, hour):
-    cur.execute(f"SELECT {select_clause} FROM " + dbname + " ORDER BY id DESC LIMIT %s;", (hour,))
-    return cur.fetchall()
-
-
-def convert_to_json(results, columns_to_select):
-    rows = []
-    for row in results:
-        row_dict = dict(zip(columns_to_select, row))
-        rows.append(row_dict)
-    return rows
 
 
 # 返回实时数据或者设备属性等等
