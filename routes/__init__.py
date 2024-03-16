bb207158a71512881154d7a5fa92bd2ad48f672d
小新 <782446723@qq.com>
2024-03-16 11:13:36 +0800

    refactor: 模块化user路由

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,7 +6,8 @@ from flask import Blueprint
 routes_bp = Blueprint('routes_bp', __name__, url_prefix='/agriculture')
 
 # 导入路由模块并注册到蓝图
-from routes import predict_routes
+from routes import predict_routes, user_routes
 
 # 将路由模块注册到蓝图
 routes_bp.register_blueprint(predict_routes.predict_bp)
+routes_bp.register_blueprint(user_routes.user_bp)
---
+++ routes/user_routes.py
@@ -0,0 +1,81 @@
+from flask import Blueprint
+from flask import request, jsonify
+
+from model import Users, db
+
+user_bp = Blueprint('user_bp', __name__)
+
+
+# 处理用户登录请求
+@user_bp.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    # 查询用户
+    user = Users.query.filter_by(username=username, password=password).first()
+
+    if user:
+        code = 200
+        access_token = 'bqddxxwqmfncffacvbpkuxvwvqrhln'  # 生成访问令牌的逻辑
+        message = '成功'
+    else:
+        code = 500
+        access_token = None
+        message = '用户名或密码错误'
+
+    response_data = {
+        'code': code,
+        'data': {'access_token': access_token},
+        'message': message
+    }
+
+    return jsonify(response_data)
+
+
+# 处理用户注册请求
+@user_bp.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    # 检查用户名是否已存在
+    existing_user = Users.query.filter_by(username=username).first()
+    if existing_user:
+        code = 500
+        message = '用户名已存在'
+    else:
+        # 创建新用户
+        new_user = Users(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        code = 200
+        message = '注册成功'
+
+    # 构建注册响应 JSON
+    response_data = {
+        'code': code,
+        'message': message
+    }
+
+    return jsonify(response_data)
+
+
+# 处理用户注销请求
+@user_bp.route('/logout', methods=['POST'])
+def logout():
+    return jsonify({"code": 200, "message": "成功"})
+
+
+# 处理用户数据导入请求
+@user_bp.route('/user/import', methods=['POST'])
+def import_data():
+    return jsonify({"code": 200, "message": "成功"})
+
+
+# 处理用户数据导出请求
+@user_bp.route('/user/export', methods=['POST'])
+def export_data():
+    return jsonify({"code": 200, "message": "成功"})
