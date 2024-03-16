0c807bfb3249cbcdf5dc83ec0d52de41cf620526
小新 <782446723@qq.com>
2024-03-16 13:44:31 +0800

    refactor: 删除无用代码

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import logging
 
-from flask import Blueprint
 from flask import Flask
 from flask_cors import CORS
 
@@ -17,12 +16,8 @@ app.config.from_object(Config)
 db.init_app(app)
 cors = CORS(app)
 
-# 创建名为"agriculture_bp"的Blueprint，并指定前缀
-agriculture_bp = Blueprint('agriculture', __name__, url_prefix='/agriculture')
-
 if __name__ == "__main__":
     # 注册Blueprint到Flask应用中
     app.register_blueprint(routes_bp)
-    app.register_blueprint(agriculture_bp)
     app.run(debug=True, host="0.0.0.0", port=5000)
-    print('the db is closed')
+    print('the server is closed')
